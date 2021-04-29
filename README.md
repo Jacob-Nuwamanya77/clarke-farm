99ba3147c0c3241376b7da9ee2d98d1f4b38196c
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-04-29 11:38:14 +0300

    remove old files

--- README.md
+++
@@ -1,2 +0,0 @@
-# clarke-farm
-A destination for the active adventurous traveler, and source of high quality Robusta Coffee.
--- backend/db.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  USER: "postgres",
-  PASSWORD: "Jimakas@123",
-  DB: "clarke-farm",
-  DIALECT: "postgres"
-};
