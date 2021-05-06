cd2ea4aab0df4bd1d4d54cdad47b604e1bbb8613
Tashmax19 <tashma19@gmail.com>
2021-05-06 08:13:04 -0700

    Update - add ourfounder and ouroffers components

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 library.add(faPhone, faBars);
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,18 +2,21 @@
   <div>
     <HeroLandingPage/>
     <AboutSection/>
+     <OurFounder/>
   </div>
 </template>
 
 <script>
 import HeroLandingPage from '@/components/HeroLanding.vue';
 import AboutSection from '@/components/AboutSection.vue';
+import OurFounder from '@/components/OurFounder.vue';
 
 export default {
   name: 'HomePage',
   components: {
     HeroLandingPage,
     AboutSection,
+    OurFounder,
   },
 };
 </script>
