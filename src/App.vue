c87d325b0989905a47dada4b1399c3e0e8eff170
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-05-07 16:11:17 +0300

    add footer

--- src/App.vue
+++ src/App.vue
@@ -18,6 +18,42 @@
       </span>
   </div>
   <router-view />
+  <footer>
+    <div class="top-row">
+      <div class="sub-container" id="contact">
+        <h5>CONTACT</h5>
+        <div class="content">
+          <p>+256 (0) 392 201 400</p>
+          <p>Plot 8244, Bukasa - Muyenga</p>
+          <p>Kampala, Uganda</p>
+        </div>
+      </div>
+      <div class="sub-container" id="sitemap">
+        <h5>SITEMAP</h5>
+        <div class="content">
+          <p><a href="">Home</a></p>
+          <p><a href="">Visit Us</a></p>
+          <p><a href="">Buy Coffee</a></p>
+          <p><a href="">Training Programs</a></p>
+        </div>
+      </div>
+      <div class="sub-container" id="location">
+        <h5>LOCATION</h5>
+        <div class="content">
+          <p>Katambale, Kyenjojo</p>
+          <p>Uganda</p>
+        </div>
+        <p>Book A Trip</p>
+      </div>
+    </div>
+    <div class="container-icons">
+      <div class="icons">
+        <span id="facebook"></span>
+        <span id="instagram"></span>
+        <span id="twitter"></span>
+      </div>
+    </div>
+  </footer>
   </div>
 </template>
 
@@ -160,5 +196,37 @@ body {
   color:var(--dark-green);
   display:flex;
   align-items:center;
+}
+footer{
+  margin-top: 70px;
+  min-height:250px;
+  background-color: var(--dark-green);
+  color:white;
+}
+.top-row{
+  display:flex;
+  flex-direction: row;
+  flex-wrap:wrap;
+  padding-top: 10px;
+  width:90%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.content{
+  margin-top: 20px;
+}
+.sub-container{
+  width:25%;
+  margin-top: 10px;
+  /* border:1px solid white; */
+}
+#sitemap{
+  margin-left: 15%;
+}
+#location{
+  margin-left: 10%;
+}
+.container-icons{
+  border-top:2px solid white;
 }
 </style>
