236ef04b294edf7cecb374ebb04fb48e30af3314
Tashmax19 <tashma19@gmail.com>
2021-05-15 19:53:57 -0700

    update on main - import more icons

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@ import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPhone, faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
+import { faTrash,faCog,faHome,faBook } from "@fortawesome/free-solid-svg-icons";
 import App from './App.vue';
 import router from './router';
 import store from './store';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-library.add(faPhone, faBars, faFacebookF, faTwitter, faInstagram);
+library.add(faPhone, faBars, faFacebookF, faTwitter, faInstagram,faTrash,faCog,faHome,faBook);
 
 createApp(App)
   .component('fa', FontAwesomeIcon)
