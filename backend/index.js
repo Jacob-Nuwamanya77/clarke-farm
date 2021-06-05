ed55cda33ecf8f92211457a1bb12d0c8ee210303
Tashmax19 <tashma19@gmail.com>
2021-06-05 09:15:12 -0700

    change port server settingd

--- backend/index.js
+++ backend/index.js
@@ -34,6 +34,7 @@ app.get('*', (req, res) => {
 });
 
 // SERVER LISTENING TO REQUESTS
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server Running on port ${port}`);
 });
