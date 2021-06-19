fff67bd43abf8100ec3b6c918fcbe294c9192a44
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-06-19 07:43:28 +0300

    fix linting errors in main.js

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import { createApp } from 'vue';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import {
-  faPhone, faEye, faBars, faTrash, faCog, faHome, faBook, faUserCog , faTrashAlt, faBookOpen, faStar, faSignOutAlt, faRunning,
-  faComments, faBell, faUser, faPlus, faEdit, faAngleLeft, faCogs, faStarAndCrescent, faAngleRight, faHamburger,
-  faStarHalfAlt,
+  faPhone, faEye, faBars, faTrash, faCog, faHome, faBook, faUserCog,
+  faTrashAlt, faBookOpen, faStar, faSignOutAlt, faRunning, faComments, faBell, faUser,
+  faPlus, faEdit, faAngleLeft, faCogs, faStarAndCrescent, faAngleRight, faHamburger,
+  faStarHalfAlt, faCheckCircle,
 } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { faFacebookF, faTwitter, faInstagram } from '@fortawesome/free-brands-svg-icons';
 import VueSweetalert2 from 'vue-sweetalert2';
-import 'sweetalert2/dist/sweetalert2.min.css';
 import App from './App.vue';
 import router from './router';
 import store from './store';
@@ -18,7 +18,8 @@ import 'sweetalert2/dist/sweetalert2.min.css';
 // import 'vue-material/dist/vue-material.min.css';
 // import 'vue-material/dist/theme/default.css';
 
-library.add(faPhone, faBars, faEye, faFacebookF, faTwitter, faCogs, faTrashAlt, faUserCog, faStarAndCrescent, faHome, faBookOpen,
+library.add(faPhone, faBars, faEye, faFacebookF, faTwitter, faCogs, faTrashAlt,
+  faUserCog, faStarAndCrescent, faHome, faBookOpen,
   faInstagram, faTrash, faCog, faHome, faBook, faStar, faSignOutAlt,
   faComments, faBell, faUser, faPlus, faEdit, faAngleLeft, faAngleRight, faRunning,
   faHamburger, faStarHalfAlt, faCheckCircle);
