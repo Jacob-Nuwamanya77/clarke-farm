951efba07ca4b13a6a4c8b4d922fc8b01b6f9b24
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-07-04 18:23:33 +0300

    upload test data for activities.

--- backend/Controllers/ActivityRoutes.js
+++ backend/Controllers/ActivityRoutes.js
@@ -36,26 +36,4 @@ router.post('/', uploads.single('file'), async (req, res) => {
   }
 });
 
-router.put('/update-activity/:id', async (req, res) => {
-  try {
-    await Activity.findOneAndUpdate({ _id: req.params.id }, req.body,
-      (err) => {
-        if (err) res.send(err);
-        res.json('Activity Updated');
-      });
-  } catch (error) {
-    res.status(400).send('Unable to update the record from the database');
-  }
-});
-
-// DELETE ACTIVITY
-router.get('/delete-activity/:id', async (req, res) => {
-  try {
-    await Activity.deleteOne({ _id: req.params.id });
-    res.json('Activity Deleted');
-  } catch (error) {
-    res.status(400).send('Unable to delete the record from the database');
-  }
-});
-
 module.exports = router;
