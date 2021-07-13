4e2846089f6265caf085972cfae504204a6dc19e
marieblessed <57671889+marieblessed@users.noreply.github.com>
2021-07-13 19:45:24 +0300

    update LoginPage

--- src/components/dashboard/LoginPage.vue
+++ src/components/dashboard/LoginPage.vue
@@ -1,8 +1,14 @@
 <template>
   <!-- eslint-disable-next-line vue/max-attributes-per-line -->
-  <div class="main-container">
+<div class="main-container">
 <div class="auth-container">
+  <div class="new-user">
+  </div>
+<div class="main-body">
 <h4 class="text-center">Sign In</h4>
+<img src="@/assets/images/Login/siginImage.png"  class="center">
+<div>
+</div>
 <form>
 <div class="form-group">
 <label class="mb-1"><strong>Email</strong></label>
@@ -15,43 +21,84 @@
 <div class="form-row d-flex justify-content-between mt-4 mb-2">
 <div class="form-group">
 <div class="custom-control custom-checkbox ml-1">
-<input type="checkbox" class="custom-control-input" id="basic_checkbox_1">
-<label class="custom-control-label" for="basic_checkbox_1">Keep me Logged in</label>
 </div>
-</div>
-<div class="form-group">
-<a>Forgot Password?</a>
 </div>
 </div>
 <div>
-<button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+  <button type="button" class="btn btn-success">Sign In</button>
 </div>
 </form>
+</div>
+<div class="copy-rights">
+ <hr class="line">
+ <p>&copy; 2021  Clarke Farm.</p>
+ </div>
 </div>
 </div>
 </template>
 
 <style scoped>
 .main-container{
-margin-top:150px;
+/* margin-top:150px; */
 display: flex;
+align-items: center;
 justify-content: center;
+min-height: 100vh;
+background-color: var(- -smoky-white)
+;
+  /* background-position: center;
+  background-repeat: no-repeat; */
+  width:100%;
 }
 .auth-container{
-width:50%;
-border: 1px solid #068d68;
+width:80%;
+background-color:white;
 border-radius: 10px;
-height:100%;
+height:500px;
 align-items: center;
 padding:20px 60px;
+}
+.main-body{
+  width:40%;
+  margin: auto;
+  margin-top: 10%;
+}
+img{
+  width:30;
+  height:30;
+  align-items: center;
 }
 h4{
   margin-bottom:20px;
   font-weight: bold;
   color:#2c3e50;
 }
+.center {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 30px;
+}
 .auth-container .form-group a{
 color:#068d68;
+}
+.form-group{
+  padding: 4px;
+}
+button{
+width: 357px;
+}
+.copy-rights{
+margin-top: 5%;
+text-align: center;
+font-weight: bold;
+}
+hr{
+color: green;
+box-shadow: grey;
+}
+.line{
+  width: 910px;
 }
 </style>
 <script>
