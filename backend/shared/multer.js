9fb901c70bb5af1c5d798e50fbbfbd7d18967c6f
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-07-14 09:24:09 +0300

    setup public directory for static files.

--- backend/shared/multer.js
+++ backend/shared/multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = function processImage(directory) {
   const storage = multer.diskStorage({
     destination(req, file, callback) {
-      callback(null, `./uploads/${directory}`);
+      callback(null, `./public/uploads/${directory}`);
     },
     filename(req, file, callback) {
       callback(null, `${Date.now()}${path.extname(file.originalname)}`);
