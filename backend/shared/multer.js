73289168e4abc078fadaec0b62eacfbe48d0d574
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-07-15 09:13:40 +0300

    change multer settings.

--- backend/shared/multer.js
+++ backend/shared/multer.js
@@ -5,7 +5,7 @@ const path = require('path');
 module.exports = function processImage() {
   const storage = multer.diskStorage({
     destination(req, file, callback) {
-      callback(null, './public/uploads/');
+      callback(null, './uploads');
     },
     filename(req, file, callback) {
       callback(null, `${Date.now()}${path.extname(file.originalname)}`);
