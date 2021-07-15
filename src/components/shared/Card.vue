d0e135009271e4296b5bf4f114085e304e0f2bc3
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-07-15 08:27:49 +0300

    Clean up store.

--- src/components/shared/Card.vue
+++ src/components/shared/Card.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="card-item">
     <div class="card-image-container">
-      <img :src="item.img" aria-hidden="true">
+      <img :src="createImagePath(item.filename)" aria-hidden="true">
     </div>
     <div class="card-content">
       <p class="card-title">{{ capitalizeEachWord(item.title) }}</p>
@@ -27,6 +27,12 @@ export default {
     },
   },
   mixins: [FormatText],
+  methods: {
+    createImagePath(filename) {
+      console.log(`${__dirname}uploads/${filename}`);
+      return `${__dirname}uploads/${filename}`;
+    },
+  },
 };
 </script>
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,13 @@
 import { createStore } from 'vuex';
-import * as activities from './modules/activities';
-import * as accomodations from './modules/accomodations';
-import * as foods from './modules/foods';
 import * as reviews from './modules/reviews';
 import * as projects from './modules/projects';
-import * as processing from './modules/coffee-processing';
 import * as bookings from './modules/bookings';
 import * as orders from './modules/orders';
 
 export default createStore({
   modules: {
-    activities,
-    accomodations,
-    foods,
     reviews,
     projects,
-    processing,
     bookings,
     orders,
   },
