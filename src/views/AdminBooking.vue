d7f66c2e1bd743fcbdf3b8ed3c67a279c3067c80
Mariat Ndagire <mariatndagire28@gmail.com>
2021-07-19 17:56:35 +0300

    bookings and orders

--- src/views/AdminBooking.vue
+++ src/views/AdminBooking.vue
@@ -5,11 +5,7 @@
 </template>
 
 <script>
-<<<<<<< HEAD
-import Dashboard from '@/components/dashboard/Booking/AdminBooking.vue';
-=======
-import AdminDashboard from '@/components/dashboard/booking/BookingDashboard.vue';
->>>>>>> 5750bcf7f31c6fae0aecef3dfb0f59a31ad692fe
+import AdminDashboard from '@/components/dashboard/Booking/BookingDashboard.vue';
 
 export default {
   name: 'AdminBooking',
