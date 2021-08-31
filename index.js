320b0e6718706adb89156677f7be3dca858359b4
Jacob Nuwamanya <nuwamanyajacob@gmail.com>
2021-08-31 07:15:21 +0300

    uninstall cors.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 // IMPORTING DEPENDENCIES
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors');
 const path = require('path');
 const mongoose = require('mongoose');
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
